04e933023e00cfec043f0b0c0d3bec3fbe326ca1
CahaseT <chasejtilton@gmail.com>
2026-02-28 17:10:14 -0700

    list sum, average and max

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is the grade percentage? ");
+        string percent = Console.ReadLine();
+        int grade = int.Parse(percent);
+        string letter;
+        if ( grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        if (grade >= 70)
+        {
+            Console.WriteLine("CONGRATS YOU PASSED");
+        }
+        Console.WriteLine(letter);
+
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Net;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        int MagicNumber = int.Parse(Console.ReadLine());
+        int response = 1;
+        while (response != MagicNumber)
+        {  
+        
+            Console.Write("What is your guess 1-100? ");
+            response = int.Parse(Console.ReadLine());
+            if (response > MagicNumber)
+        {
+            Console.WriteLine("TOO HIGH!");
+        }
+            else if (response < MagicNumber)
+        {
+            Console.WriteLine("TOO LOW!");
+        }
+        }
+        Console.WriteLine("GOOD JOB!");
     }
 }
